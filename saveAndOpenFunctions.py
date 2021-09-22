77eb9c7125c71bf26f4831a3d35589cb07ffe32f
cameronreidalbin <52802178+cameronreidalbin@users.noreply.github.com>
2021-09-22 17:40:13 -0500

    Updated to make functions work

--- saveAndOpenFunctions.py
+++ saveAndOpenFunctions.py
@@ -4,19 +4,16 @@ Wuts up on dem dizates y'all? Mon Sep 20 15:34:15 2021
 
 @Who dat bad man programmin? cameron.albin
 """
-
-
-#Saving File
 import pickle
-output = open('childBot.pkl','wb')
-pickle.dump(childBot, output)
-output.close()
 
-#Opening Bot
-import pickle
-pkl_file = open('C:\\Users\\cameron.albin\\.spyder-py3\\Connect4\\childBot.pkl', 'rb')
-data1 = pickle.load(pkl_file)
-print(data1[0][0])
-pkl_file.close()
+def saveBots(bots,fileName):
+    output = open(fileName,'wb')
+    pickle.dump(bots, output)
+    output.close()
 
+def loadBots(fileName):
+    pkl_file = open(fileName, 'rb')
+    data1 = pickle.load(pkl_file)
+    pkl_file.close()
+    return data1
 
